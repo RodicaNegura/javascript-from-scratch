094cc1332f97c4e9f83e3e8cc5254288d5634c8e
RodicaNegura <RodicaElenaNegura@gmail.com>
2024-01-12 14:32:11 +0000

    Creating Arrays

--- js/script.js
+++ js/script.js
@@ -8,43 +8,65 @@
 
 // Data Type (primitive and non-primitive)
 
-// 1 - Primitive
+  // 1 - Primitive
 
-  // ---- Boolean
-  // let boolean = true;
-  // console.log(typeof(boolean));
-
-
-  // ---- Null
-  // let n = null;
-  // console.log(typeof(n));
+    // ---- Boolean
+    // let boolean = true;
+    // console.log(typeof(boolean));
 
 
-  // ---- Undefined
-  // let undf = undefined;
-  // console.log(typeof(undf));
-  // let a; The variable ‘a’ has been declared but hasn’t been assigned a value yet.
-  // console.log(a); 
-  // We can assign a value to a:
-  // a = 5;
-  // console.log(a); This will return 5
+    // ---- Null
+    // let n = null;
+    // console.log(typeof(n));
 
-  // ---- Number 
-  // let num = 123;
-  // console.log(typeof(num));
 
-   // ---- String 
-  // let str = 'This is a String';
-  // console.log(str)
-  // console.log(typeof(str));
+    // ---- Undefined
+    // let undf = undefined;
+    // console.log(typeof(undf));
+    // let a; The variable ‘a’ has been declared but hasn’t been assigned a value yet.
+    // console.log(a); 
+    // We can assign a value to a:
+    // a = 5;
+    // console.log(a); This will return 5
 
+    // ---- Number 
+    // let num = 123;
+    // console.log(typeof(num));
+
+    // ---- String 
+    // let str = 'This is a String';
+    // console.log(str)
+    // console.log(typeof(str));
 
   // 1 - Non-Primitive
 
-    // ---- Object
+      // ---- Object
 
-    // let person = {
-    //   age: 30,
-    //   name: 'Bogdan',
-    // }
-    // console.log(person);
+      // let person1 = {
+      //   age: 30,
+      //   name: 'Mya',
+      // }
+      // console.log(person1);
+
+      // let person2 = {
+      //   age: 35,
+      //   name: 'Lynn',
+      // }
+
+// Arrays
+
+// const hobbies = ["Reading", "Fishing", "Cooking"];
+// console.log(hobbies);
+// const prices = [31.4, 29.4, 175.6];
+// console.log(prices);
+// const fishing = hobbies[1];
+// console.log(fishing);
+
+// const persons = [person1, person2];
+// console.log(persons);
+
+// const people = [  {id: 1, age: 35, email: "bjdkjfds@yahoo.com"},
+//                   {id: 2, age: 33, email: "gbggg@yahoo.com"}
+//                 ];
+
+// console.log(people);
