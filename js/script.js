eed809683aa90900c0b42f318020de2299be9b24
RodicaNegura <RodicaElenaNegura@gmail.com>
2024-01-12 13:25:08 +0000

    Primitive Data Types

--- js/script.js
+++ js/script.js
@@ -1,8 +1,39 @@
+// variable (let, var and const)
+// var myName = 'Mihai';
+
+// console.log(myName);
+// let secondName = 'Sandu';
+// const age = 30;
 
 
+// Data Type (primitive and non-primitive)
 
-var myName = 'Mihai';
+// 1 - Primitive
 
-console.log(myName);
-let secondName = 'Sandu';
-const age = 30;
+  // ----Boolean
+  // let boolean = true;
+  // console.log(typeof(boolean));
+
+
+  // ----Null
+  // let n = null;
+  // console.log(typeof(n));
+
+
+  // ----Undefined
+  // let undf = undefined;
+  // console.log(typeof(undf));
+  // let a; The variable ‘a’ has been declared but hasn’t been assigned a value yet.
+  // console.log(a); 
+  // We can assign a value to a:
+  // a = 5;
+  // console.log(a); This will return 5
+
+  // Number 
+  // let num = 123;
+  // console.log(typeof(num));
+
+   // String 
+  // let str = 'This is a String';
+  // console.log(str)
+  // console.log(typeof(str));
