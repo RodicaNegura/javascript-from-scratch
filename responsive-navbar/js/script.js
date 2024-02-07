337540fcd4ef42c9ef2e96e9461a465399a78ed6
RodicaNegura <RodicaElenaNegura@gmail.com>
2024-02-07 15:06:50 +0000

    functionality for project

--- responsive-navbar/js/script.js
+++ responsive-navbar/js/script.js
@@ -1,1 +1,7 @@
+const navToggle = document.querySelector(".nav-hamburger");
+const links = document.querySelector("links");
 
+navToggle.addEventListener("click", function () {
+  
+  links.classList.toggle("show-links");
+})
